66a9ce7d0d3188d0e597f1a09c77167d7a95fcc7
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-08-15 15:57:58 +0100

    getting started

--- typingGame/main.py
+++ typingGame/main.py
@@ -2,6 +2,10 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
     stdscr.clear() #clear the screen
 
     stdscr.addstr('hello world')
