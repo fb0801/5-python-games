746882f212f1faafc06ac471a3608e561eec6220
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-08-15 18:01:33 +0100

    Add files via upload

--- typingGame/main.py
+++ typingGame/main.py
@@ -1,16 +1,22 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear() #clear the screen
+
+    stdscr.addstr('Welcome to the Speed typing test')
+    stdscr.addstr('\nPress any key to begin!')
+
+    stdscr.refresh()
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    stdscr.clear() #clear the screen
-
-    stdscr.addstr('hello world')
-    stdscr.refresh()
-    stdscr.getkey()
+    key = stdscr.getkey()
+    print(key)
 
 wrapper(main)
 
