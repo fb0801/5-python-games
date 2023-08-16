45127c3bfd51867247559854d23f56501622a5af
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-08-16 17:51:46 +0100

    overlay txt 2

--- typingGame/main.py
+++ typingGame/main.py
@@ -12,6 +12,7 @@ def start_screen(stdscr):
 
 def  display_text(stdscr, target, current, wpm=0):
     stdscr.addstr(target)
+    stdscr.addstr(1,0 , f"WPM: {wpm}")
 
     for i, char in enumerate(current):
         correct_char = target[i]
@@ -26,11 +27,12 @@ def wpm_test(stdscr):
                    'Fly relate house expert charge interview itself because job consider knowledge color low late hope significant understand business home where entire tonight want heavy such sell way employee by civil hold executive become station successful enough task exacFtly reflect about fear let perform term always industry spend feeling play federal performance season major buy ability evidence treat wall true like project return popular whether inside especially say size fast really activity final use strategy maintain see add explain conference school line almost economy rise various claim range imagine their central watch art right century scientist thought radio rule call administration light concern pick coach make chair suddenly information show rock pretty ready hang finally music cold join professional later though series head college building career consumer everyone sure area maybe history wear land matter save realize family plan risk compare prepare simply meet last however score rest card also bring begin movement moment material night reduce these live condition yeah food than morning city speak enjoy laugh teacher cell health well summer player interesting might subject movie themselves price trip address anything million get image probably recent why reveal billion write hair may remove car response just']
     target_text = 'hello this is some text'
     current_text= []
+    wpm = 0
    
     while True:
         stdscr.clear() #clear the screen
         
-        display_text(stdscr, target_text, current_text)
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
         key = stdscr.getkey()
@@ -41,7 +43,7 @@ def wpm_test(stdscr):
             if len(current_text) > 0:
                 current_text.pop()
 
-            else:
+            elif len(current_text) < len(target_text):
                 current_text.append(key)
 
         current_text.append(key)
