3aa098a29dfdd5402afc971ea4eba832814a8d82
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-08-16 18:19:14 +0100

    final touches

--- typingGame/main.py
+++ typingGame/main.py
@@ -1,6 +1,7 @@
 import curses
 from curses import wrapper
 import time
+import random
 
 def start_screen(stdscr):
     stdscr.clear() #clear the screen
@@ -23,10 +24,15 @@ def  display_text(stdscr, target, current, wpm=0):
 
         stdscr.addstr(0, i, char, color)
 
+def load_text():
+    with open('text.txt', 'r') as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+
+
+
 def wpm_test(stdscr):
-    tget_text = ['hello world this is some text','To learn to type quickly, practice often and adopt the proper technique. This will help you develop muscle memory and create automatic reflexes. Keep practicing and gradually pick up the pace. You will see results after just a few weeks!', 
-                   'Fly relate house expert charge interview itself because job consider knowledge color low late hope significant understand business home where entire tonight want heavy such sell way employee by civil hold executive become station successful enough task exacFtly reflect about fear let perform term always industry spend feeling play federal performance season major buy ability evidence treat wall true like project return popular whether inside especially say size fast really activity final use strategy maintain see add explain conference school line almost economy rise various claim range imagine their central watch art right century scientist thought radio rule call administration light concern pick coach make chair suddenly information show rock pretty ready hang finally music cold join professional later though series head college building career consumer everyone sure area maybe history wear land matter save realize family plan risk compare prepare simply meet last however score rest card also bring begin movement moment material night reduce these live condition yeah food than morning city speak enjoy laugh teacher cell health well summer player interesting might subject movie themselves price trip address anything million get image probably recent why reveal billion write hair may remove car response just']
-    target_text = 'hello this is some text'
+    target_text = load_text()
     current_text= []
     wpm = 0
     start_time = time.time()
